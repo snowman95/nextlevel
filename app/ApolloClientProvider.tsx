be41b2737143c07fddfb4161a0ae5f4004ab4cad
snowman95 <hun2ya9@gmail.com>
2023-02-13 10:32:07 +0900

    서버 사이드 렌더링 지원하기 위한 ApolloClient 설정 파일과 Provider 컴포넌트를 추가하라

---
+++ app/ApolloClientProvider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "./ApolloClient";
+
+type Props = {
+  initialApolloState: string;
+  children: React.ReactNode;
+};
+
+export default function ApolloClientProvider(props: Props) {
+  const apolloClient = useApollo(JSON.parse(props.initialApolloState));
+  return (
+    <ApolloProvider client={apolloClient}>{props.children}</ApolloProvider>
+  );
+}
