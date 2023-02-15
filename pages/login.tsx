8e50f532d2729ec97076c8251f74468c413bec1d
snowman95 <hun2ya9@gmail.com>
2023-02-15 22:10:26 +0900

    /login 페이지에 카카오 로그인 기능 추가하라

---
+++ pages/login.tsx
@@ -0,0 +1,22 @@
+// pages/login.tsx
+
+import { signIn, useSession, signOut } from "next-auth/react";
+
+export default function Home() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        {session.user?.name}님 반갑습니다 <br />
+        <button onClick={() => signOut()}>로그아웃</button>
+      </>
+    );
+  }
+  return (
+    <>
+      로그인되지 않았습니다 <br />
+      <button onClick={() => signIn("kakao")}>로그인</button>
+    </>
+  );
+}
