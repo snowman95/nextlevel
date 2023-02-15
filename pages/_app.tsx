6eead70712221ccb8208da5cdafbd26fd352a00e
snowman95 <hun2ya9@gmail.com>
2023-02-15 21:17:18 +0900

    _app.tsx 에 Layout 적용 코드를 추가하라

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,27 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
-import { client } from "@/config/gqlClient";
+import { useApollo } from "@/config/gqlClient";
+import ClientOnly from "@/ui/ClientOnly";
+import { NextPage } from "next";
+import { ReactElement, ReactNode } from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const apolloClient = useApollo(pageProps);
+
+  // 각 페이지에 레이아웃이 있다면 넣어주는 코드입니다.
+  const getLayout = Component.getLayout ?? ((page) => page);
+
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
+    <ApolloProvider client={apolloClient}>
+      <ClientOnly>{getLayout(<Component {...pageProps} />)}</ClientOnly>
     </ApolloProvider>
   );
 }
