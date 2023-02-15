8351fc9be308fd7af2d236c02b15f959fba88a3f
snowman95 <hun2ya9@gmail.com>
2023-02-15 10:50:44 +0900

    pages 디렉토리 기본 파일 생성하라

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,6 @@
 export default function Custom404() {
   return (
     <div>
-      <p>Sorry , The page you are looking for can't be found</p>
       <p>Try checking your URL</p>
       <h2>
         This is a <span style={{ color: "red" }}>404 page</span>
