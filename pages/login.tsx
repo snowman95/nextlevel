28a23808f0781ddcf36e867b1bf0e0cb85c2eda8
snowman95 <hun2ya9@gmail.com>
2023-02-17 14:32:23 +0900

    /login 페이지에 네이버 로그인 기능을 추가하라

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,6 +4,8 @@ import { signIn, useSession, signOut } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
+  console.log("session", session);
+
 
   if (session) {
     return (
@@ -16,7 +18,8 @@ export default function Home() {
   return (
     <>
       로그인되지 않았습니다 <br />
-      <button onClick={() => signIn("kakao")}>로그인</button>
+      <button onClick={() => signIn("kakao")}>카카오 로그인</button>
+      <button onClick={() => signIn("naver")}>네이버 로그인</button>
     </>
   );
 }
