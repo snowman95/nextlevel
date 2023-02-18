b1d6dbb894629c0918852d79a3e26915ec465657
snowman95 <hun2ya9@gmail.com>
2023-02-18 13:06:09 +0900

    첫 페이지에 로그인 페이지 링크 추가하라

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,19 @@ import Link from "next/link";
 export default function IndexPage() {
   return (
     <div className="flex flex-col justify-center w-full h-full gap1">
-      <Link href={"/buca"} className="bg-blue-200 border w-fit">
+      <Link href={"/login"} className="border w-fit">
+        로그인
+      </Link>
+      <Link href={"/buca"} className="border w-fit">
         뿌카
       </Link>
-      <Link href={"/photos"} className="bg-blue-300 border w-fit">
+      <Link href={"/photos"} className="border w-fit">
         이미지들
       </Link>
-      <Link href={"/posts"} className="bg-blue-300 border w-fit">
+      <Link href={"/posts"} className="border w-fit">
         게시물들
       </Link>
-      <Link href={"/animals"} className="bg-blue-300 border w-fit">
+      <Link href={"/animals"} className="border w-fit">
         동물들
       </Link>
     </div>
